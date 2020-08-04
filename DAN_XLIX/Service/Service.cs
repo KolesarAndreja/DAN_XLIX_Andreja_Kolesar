03b74be5cf756b10dc32fcacdd3da1205f453957
KolesarAndreja <kolesar.andreja@gmail.com>
2020-08-04 14:55:26 +0200

    EF added, Owner View and ViewModel implemented

---
+++ DAN_XLIX/Service/Service.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XLIX.Service
+{
+    class Service
+    {
+
+        #region GET ROLE
+        public static string GetRole(int id)
+        {
+            try
+            {
+                using (dbHotelEntities context = new dbHotelEntities())
+                {
+                    tblStaff res = (from x in context.tblStaffs where x.userId == id select x).FirstOrDefault();
+
+                    if (res != null)
+                    {
+                        return "staff";
+                    }
+                    else 
+                    {
+                        return "manager";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception " + ex.Message.ToString());
+                return null;
+            }
+        }
+        #endregion
+
+        #region FIND USER BY USERNAME AND PASSWORD
+
+        public static tblUser IsValidUser(string username, string password)
+        {
+            try
+            {
+                using (dbHotelEntities context = new dbHotelEntities())
+                {
+                    tblUser res = (from x in context.tblUsers where x.username==username && x.password == password select x).FirstOrDefault();
+                    return res;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception " + ex.Message.ToString());
+                return null;
+            }
+        }
+        #endregion
+
+        #region ADD USER
+        public static tblUser AddUser(tblUser user)
+        {
+            try
+            {
+                using (dbHotelEntities context = new dbHotelEntities())
+                {
+                    if (user.userId == 0)
+                    {
+                        //add 
+                        tblUser newUser = new tblUser();
+                        newUser.username = user.username;
+                        newUser.password = user.password;
+                        newUser.dateOfBirth = user.dateOfBirth;
+                        newUser.email = user.email;
+                        newUser.fullname = user.fullname;
+                        context.tblUsers.Add(newUser);
+                        context.SaveChanges();
+                        user.userId = newUser.userId;
+                        return user;
+                    }
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message.ToString());
+                return null;
+            }
+        }
+        #endregion
+
+        #region ADD MANAGER
+        public static tblManager AddManager(tblManager manager)
+        {
+            try
+            {
+                using (dbHotelEntities context = new dbHotelEntities())
+                {
+                    if (manager.managerId == 0)
+                    {
+                        //add 
+                        tblManager newManager = new tblManager();
+                        newManager.floorNumber = manager.floorNumber;
+                        newManager.workExperience = manager.workExperience;
+                        newManager.qualificationId = manager.qualificationId;
+                        context.tblManagers.Add(newManager);
+                        context.SaveChanges();
+                        manager.managerId = newManager.managerId;
+                        return manager;
+                    }
+                    return manager;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message.ToString());
+                return null;
+            }
+        }
+        #endregion
+
+        #region ADD STAFF
+        public static tblStaff AddStaff(tblStaff staff)
+        {
+            try
+            {
+                using (dbHotelEntities context = new dbHotelEntities())
+                {
+                    if (staff.staffId == 0)
+                    {
+                        //add 
+                        tblStaff newStaff = new tblStaff();
+                        newStaff.citizenship = staff.citizenship;
+                        newStaff.floorNumber = staff.floorNumber;
+                        newStaff.engegamentId = staff.engegamentId;
+                        newStaff.genderId = staff.genderId;
+                        context.tblStaffs.Add(newStaff);
+                        context.SaveChanges();
+                        staff.staffId = newStaff.staffId;
+                        return staff;
+                    }
+                    return staff;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message.ToString());
+                return null;
+            }
+        }
+        #endregion
+    }
+}
---
+++ DAN_XLIX/ViewModel/OwnerViewModel.cs
@@ -0,0 +1,293 @@
+﻿using DAN_XLIX.Command;
+using DAN_XLIX.Service;
+using DAN_XLIX.View;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DAN_XLIX.ViewModel
+{
+    class OwnerViewModel:ViewModelBase
+    {
+        #region Prop
+        Owner owner;
+        private tblUser _newEmployee;
+        public tblUser newEmployee
+        {
+            get
+            {
+                return _newEmployee;
+            }
+            set
+            {
+                _newEmployee = value;
+                OnPropertyChanged("newEmployee");
+            }
+        }
+
+        private tblManager _newManager;
+        public tblManager newManager
+        {
+            get
+            {
+                return _newManager;
+            }
+            set
+            {
+                _newManager = value;
+                OnPropertyChanged("newManager");
+            }
+        }
+
+        private tblStaff _newStaff;
+        public tblStaff newStaff
+        {
+            get
+            {
+                return _newStaff;
+            }
+            set
+            {
+                _newStaff = value;
+                OnPropertyChanged("newStaff");
+            }
+        }
+        #endregion
+
+        #region constructor
+        public OwnerViewModel(Owner openOwner)
+        {
+            owner = openOwner;
+            newEmployee = new tblUser();
+            newManager = new tblManager();
+            newStaff = new tblStaff();
+        }
+        #endregion
+
+        #region Visibility
+        private Visibility _viewStaff = Visibility.Collapsed;
+        public Visibility viewStaff
+        {
+            get
+            {
+                return _viewStaff;
+            }
+            set
+            {
+                _viewStaff = value;
+                OnPropertyChanged("viewStaff");
+            }
+        }
+        private Visibility _viewManager = Visibility.Collapsed;
+        public Visibility viewManager
+        {
+            get
+            {
+                return _viewManager;
+            }
+            set
+            {
+                _viewManager = value;
+                OnPropertyChanged("viewManager");
+            }
+        }
+        private ICommand _createStaff;
+        public ICommand createStaff
+        {
+            get
+            {
+                if (_createStaff == null)
+                {
+                    _createStaff = new RelayCommand(param => CreateStuffExecute(), param => CanCreateStuffExecute());
+                }
+                return _createStaff;
+            }
+        }
+
+        private void CreateStuffExecute()
+        {
+
+            try
+            {
+                
+                if(viewManager == Visibility.Visible)
+                {
+                    viewManager = Visibility.Collapsed;
+                }
+                viewStaff = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanCreateStuffExecute()
+        {
+            return true;
+        }
+
+        private ICommand _createManager;
+        public ICommand createManager
+        {
+            get
+            {
+                if (_createManager == null)
+                {
+                    _createManager = new RelayCommand(param => CreateManagerExecute(), param => CanCreateManagerExecute());
+                }
+                return _createManager;
+            }
+        }
+
+        private void CreateManagerExecute()
+        {
+
+            try
+            {
+                if(viewStaff == Visibility.Visible)
+                {
+                    viewStaff = Visibility.Hidden;
+                }
+                viewManager = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanCreateManagerExecute()
+        {
+            return true;
+        }
+        #endregion
+
+        #region Commands
+
+        private ICommand _save1;
+        public ICommand save1
+        {
+            get
+            {
+                if (_save1 == null)
+                {
+                    _save1 = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+                return _save1;
+            }
+        }
+
+        private void SaveExecute()
+        {
+            try
+            {
+                tblUser e = Service.Service.AddUser(newEmployee);
+                newManager.userId = e.userId;
+                tblManager me = Service.Service.AddManager(newManager);
+
+                if (e != null && me!=null)
+                {
+                    MessageBox.Show("Manager has been succesfully created!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanSaveExecute()
+        {
+            if (Service.Service.IsValidUser(newEmployee.username, newEmployee.password) == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private ICommand _save2;
+        public ICommand save2
+        {
+            get
+            {
+                if (_save2 == null)
+                {
+                    _save2 = new RelayCommand(param => Save2Execute(), param => CanSave2Execute());
+                }
+                return _save2;
+            }
+        }
+
+        private void Save2Execute()
+        {
+            try
+            {
+                tblUser e = Service.Service.AddUser(newEmployee);
+                newStaff.userId = e.userId;
+                tblStaff me = Service.Service.AddStaff(newStaff);
+
+                if (e != null && me!=null)
+                {
+                    MessageBox.Show("Staff has been succesfully created!");
+                }
+                else
+                {
+                    MessageBox.Show("Error. Try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanSave2Execute()
+        {
+            if (Service.Service.IsValidUser(newEmployee.username, newEmployee.password) == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private ICommand _logOut;
+        public ICommand logOut
+        {
+            get
+            {
+                if (_logOut == null)
+                {
+                    _logOut = new RelayCommand(param => LogOutExecute(), param => CanLogOutExecute());
+                }
+                return _logOut;
+            }
+        }
+
+        private void LogOutExecute()
+        {
+            try
+            {
+                Login login = new Login();
+                owner.Close();
+                login.Show();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private bool CanLogOutExecute()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
